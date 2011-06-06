98d81a439c64f0fe9db40b66fd93792e83ea4139
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-06 02:32:47 -0600

    Added javadoc for distance function. Added an alias for distance function using Location.
    Change-Id: I4111da212685e4c8c3761b98a7fa0a157afd8a07

--- src/pinoygamers/AngryMobs/Functions.java
+++ src/pinoygamers/AngryMobs/Functions.java
@@ -71,11 +71,32 @@ public class Functions {
         return s.toUpperCase().charAt(0) + s.toLowerCase().substring(1);
     }
     
+    /**
+     * Returns the distance between two points in a 3D world.
+     * @param x1
+     * @param y1
+     * @param z1
+     * @param x2
+     * @param y2
+     * @param z2
+     * @return Distance between 1 and 2.
+     */
     public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
     	double distance=0;
 
         distance=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)+Math.pow((z2-z1),2));
         return distance;
+    }
+    
+    /**
+     * Returns the distance between two locations. Assumes the locations
+     * are in the same world.
+     * @param a
+     * @param b
+     * @return
+     */
+    public static double distance(Location a, Location b) {
+    	return distance(a.getX(), a.getY(), a.getZ(), b.getX(), b.getY(), b.getZ());
     }
 
 }
