010e839f6a9c985ebafb7851024dfa779bec3a28
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-07 14:20:10 -0600

    Another change to make the jar compile correctly.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 javac -cp libs/craftbukkit-818.jar -sourcepath src/ -d build src/pinoygamers/AngryMobs/*
 rm AngryMobs-*.jar
-jar cf AngryMobs-$BUILD_NUMBER.jar build/* plugin.yml
+jar cf AngryMobs-$BUILD_NUMBER.jar plugin.yml -C build pinoygamers
 cd src
 javadoc -d "../docs/" pinoygamers/AngryMobs/*
