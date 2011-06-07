7cc7a24613715497650afedad0ae5eb9cdfcbf7a
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-07 14:17:11 -0600

    Edited build.sh for jenkins autobuilder

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 javac -cp libs/craftbukkit-818.jar -sourcepath src/ -d build src/pinoygamers/AngryMobs/*
-jar cf AngryMobs-$BUILD_NUMBER build/* plugin.yml
+rm AngryMobs-*.jar
+jar cf AngryMobs-$BUILD_NUMBER.jar build/* plugin.yml
 cd src
 javadoc -d "../docs/" pinoygamers/AngryMobs/*
