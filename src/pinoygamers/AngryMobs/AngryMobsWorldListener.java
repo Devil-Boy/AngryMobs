db60d2782b6b2bcc3cab7428c00523ef5572bb53
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-06 22:48:58 -0600

    Added config loading.

---
+++ src/pinoygamers/AngryMobs/AngryMobsWorldListener.java
@@ -0,0 +1,23 @@
+package pinoygamers.AngryMobs;
+
+import java.io.File;
+
+import org.bukkit.event.world.WorldListener;
+import org.bukkit.event.world.WorldLoadEvent;
+
+public class AngryMobsWorldListener extends WorldListener {
+	
+	private AngryMobs plugin;
+
+	public AngryMobsWorldListener(AngryMobs plugin) {
+		this.plugin = plugin;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void onWorldLoad( WorldLoadEvent event )
+	{
+		File conffile = new File(plugin.pluginMainDir + "/" + plugin.configPrefix + event.getWorld().getName() + ".ini");
+		plugin.worldConfigs.put(event.getWorld().getName(), new Configuration(conffile));
+	}
+
+}
