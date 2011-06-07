993d1c0b5bca51ea75d9df2276b96e1b8f62bc56
Miika <vashthetempes@hotmail.com>
2011-06-07 10:50:56 +0300

    Random location returns location of block ABOVE ground instead of highest ground block.
    Change-Id: Ia1e838035f8fb247d7b95d50edd8e82915ccfcd7

--- src/pinoygamers/AngryMobs/Functions.java
+++ src/pinoygamers/AngryMobs/Functions.java
@@ -171,7 +171,7 @@ public class Functions {
     	java.util.Random generator = new java.util.Random();
     	int randX = c.getX() + generator.nextInt(16);
     	int randZ = c.getZ() + generator.nextInt(16);
-    	int Y = c.getWorld().getHighestBlockYAt(randX, randZ);
+    	int Y = c.getWorld().getHighestBlockYAt(randX, randZ)+1;
     	
     	return new Location(c.getWorld(), randX, Y, randZ);
     }
