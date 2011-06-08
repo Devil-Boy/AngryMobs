97a0bfaace0c5afea7ef68a4039c13ed3aed976e
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-08 01:33:20 -0600

    Added Findbugs code in build.sh.
    Change-Id: I1c7fa4da8a1b731e6686e5d3d4f278b65c85023e

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ rm AngryMobs-*.jar
 jar cf AngryMobs-$BUILD_NUMBER.jar plugin.yml -C build pinoygamers
 cd src/
 javadoc -private -d "../docs/" pinoygamers/AngryMobs/*
+/home/joshua/Documents/findbugs-1.3.9/bin/findbugs -textui -effort:max -low -xml:withMessages -output "findbugs.xml" -auxclasspath "$WORKSPACE/libs/craftbukkit-818.jar" -sourcepath "$WORKSPACE/src" $WORKSPACE/build
