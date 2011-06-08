1cd958e9d0bf6dc13a81d95d0c5c507c5c8dcc51
Devil boy <Kervin@Kervin-Laptop>
2011-06-08 12:32:38 -0700

    Remove spawned mobs from list...

--- src/pinoygamers/AngryMobs/AngryMobsEntityListener.java
+++ src/pinoygamers/AngryMobs/AngryMobsEntityListener.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityListener;
@@ -25,12 +26,13 @@ public class AngryMobsEntityListener extends EntityListener {
 
     public void onCreatureSpawn(CreatureSpawnEvent event) {
     	LivingEntity theBorn = (LivingEntity) event.getEntity();
-    	if (!plugin.mobSpawns.contains(theBorn) && plugin.worldConfigs.get(theBorn.getWorld().getName()).disableNormalMonsters) {
+    	if ((!plugin.mobSpawns.contains(theBorn)) && plugin.worldConfigs.get(theBorn.getWorld().getName()).disableNormalMonsters && (theBorn instanceof Monster)) {
     		event.setCancelled(true);
     		if (plugin.debug) {
     			System.out.println("Normal Spawn canceled!");
     		}
     	}
+    	plugin.mobSpawns.removeLastOccurrence(theBorn);
     }
 }
 
