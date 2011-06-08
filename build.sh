2601a9587637e9e1ed5fbbb0daef4f63e194bba2
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-08 00:52:09 -0600

    Fixed build.sh permissions.
    Change-Id: I3ab71c38e9c850a619c6b99e908fdd4a1d00d133

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 javac -cp libs/craftbukkit-818.jar -sourcepath src/ -d build src/pinoygamers/AngryMobs/*
 rm AngryMobs-*.jar
 jar cf AngryMobs-$BUILD_NUMBER.jar plugin.yml -C build pinoygamers
-cd src
+cd src/
 javadoc -private -d "../docs/" pinoygamers/AngryMobs/*
