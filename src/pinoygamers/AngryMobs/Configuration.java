83bbddc16632d6e64e0d86340610af9573529f1b
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-10 00:04:57 -0600

    Fixed list being duplicated
    Change-Id: I25f81c414faf59626c9b185059f653c4925a3a2d

--- src/pinoygamers/AngryMobs/Configuration.java
+++ src/pinoygamers/AngryMobs/Configuration.java
@@ -245,9 +245,8 @@ public class Configuration implements java.io.Serializable {
 	 * @return A parsed LinkedList.
 	 */
     public LinkedList<String> ratioizeList(LinkedList<String> list) {
-    	LinkedList outputList = list;
-    	for (int i=0; i<list.size(); i++) {
-    		String currentValue = list.get(i);
+    	LinkedList<String> outputList = new LinkedList<String>();
+    	for (String currentValue : list) {
     		if (currentValue.contains(":")) {
     			String[] theValues = currentValue.split(":");
     			int theMultiple;
@@ -260,8 +259,11 @@ public class Configuration implements java.io.Serializable {
         			} catch (NumberFormatException nfe) {
         				System.out.println(theValues[1] + " is not a number...");
         			}
+    			}else {
+    				outputList.add(theValues[0]);
     			}
-    			outputList.removeFirstOccurrence(currentValue);
+    		}else {
+    			outputList.add(currentValue);
     		}
     		if (debug) {
     			System.out.println("Current spawnableMonsters: " + linkedListToString(list));
