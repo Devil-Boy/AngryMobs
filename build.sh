72d8835ffdb3d12b02e5f040dacd631696c3f0ea
Joshua Reetz <josh100288@jrtechsupport.com>
2011-06-15 20:47:25 -0600

    Changed craftbukkit dependency to 860 so we can implement onWorldUnload.
    Change-Id: Id8add73b2716aabd81fa6c5a6bf83d20140a56bf

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-javac -cp libs/craftbukkit-818.jar -sourcepath src/ -d build src/pinoygamers/AngryMobs/*
+javac -cp libs/craftbukkit-860.jar -sourcepath src/ -d build src/pinoygamers/AngryMobs/*
 rm AngryMobs-*.jar
 jar cf AngryMobs-$BUILD_NUMBER.jar plugin.yml -C build pinoygamers
 cd src/
